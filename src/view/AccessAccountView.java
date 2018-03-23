9432cfcbfc0b756560741c3536f95db4011b267e
Omid - Acer <nassiromid@gmail.com>
2018-03-22 17:14:07 -0700

    Initial commit

---
+++ src/view/AccessAccountView.java
@@ -0,0 +1,81 @@
+package view;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class AccessAccountView extends Application
+{
+
+    TextArea results;
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    public void start(Stage window)
+    {
+        Scene displayScreen = getDisplayScreenScene();
+
+        window.setScene(displayScreen);
+        window.show();
+
+        results.appendText("Please make a selection \n");
+    }
+
+    private Scene getDisplayScreenScene()
+    {
+        BorderPane frame = new BorderPane();
+
+        //Buttons
+        HBox buttonsPanel = new HBox();
+        buttonsPanel.setAlignment(Pos.BASELINE_CENTER);
+
+        Button depositButton = new Button("Deposit");
+        depositButton.setOnAction( e -> results.appendText("Enter an amount to deposit \n"));
+
+        Button withdrawalButton = new Button("Withdrawal");
+        withdrawalButton.setOnAction( e -> results.appendText("Enter an amount to withdrawal \n"));
+
+        Button checkAcctButton = new Button("Check Account");
+
+        Button logOutButton = new Button("Log Out");
+
+        buttonsPanel.getChildren().addAll(depositButton, withdrawalButton, checkAcctButton, logOutButton);
+
+        // Text area
+        results = new TextArea();
+        results.setPrefColumnCount(40);
+        results.setPrefRowCount(30);
+
+                                                        //Bottom Panel
+        HBox bottomPanel = new HBox();
+        bottomPanel.setAlignment(Pos.BASELINE_CENTER);
+
+        //Text field
+        TextField input = new TextField();
+        input.setPrefColumnCount(10);
+
+        //Button
+
+        Button submitButton = new Button("Submit");
+
+        bottomPanel.getChildren().addAll(input, submitButton);
+        frame.setTop(buttonsPanel);
+        frame.setCenter(results);
+        frame.setBottom(bottomPanel);
+
+        return new Scene(frame, 600, 500);
+    }
+
+
+}
