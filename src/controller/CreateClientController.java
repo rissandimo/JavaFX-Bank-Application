904352e673e8ca590783487de4f42bc47fa09571
= <nassiromid@gmail.com>
2018-04-03 14:19:44 -0700

    added methods:  clientInfoValid  doesAccountExist

--- src/controller/CreateClientController.java
+++ src/controller/CreateClientController.java
@@ -2,20 +2,24 @@
 package controller;
 
 import model.BankConnection;
+import view.WelcomeScreen;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import javax.swing.*;
+import java.sql.*;
 
 public class CreateClientController
 {
     private String firstName, lastName, social;
     private int accountNumber;
+    private Connection bankConnection;
+
+
+    private WelcomeScreen view;
 
     public CreateClientController(String firstName, String lastName, String social)
     {
-        Connection bankConnection = BankConnection.createConnection();
-
+        this.view = new WelcomeScreen();
+        this.bankConnection = BankConnection.createConnection();
         this.accountNumber = Utilities.getBiggestAccountNumber(bankConnection);
         this.accountNumber++; // increment account number for next client
 
@@ -28,9 +32,38 @@ public class CreateClientController
 
         private void addClientToDatabase(int accountNumber, Connection connection)
         {
-        addClientInfo(firstName, lastName, social, connection);
-        addCheckingInfo(connection, accountNumber, 0.0, social);
+           if(clientInfoValid())
+           {
+            addClientInfo(firstName, lastName, social, connection);
+            addCheckingInfo(connection, accountNumber, 0.0, social);
+
+           }
         }
+
+    private boolean clientInfoValid()
+    {
+        boolean informationCorrect = false;
+
+        social = social.replaceAll("[- ]", "");
+
+
+        if(firstName.length() == 0)
+        {
+            JOptionPane.showMessageDialog(null, "First name invalid");
+            System.out.println("first name length: " + firstName.length());
+        } else if(lastName.length() == 0)
+        {
+            JOptionPane.showMessageDialog(null, "Last name invalid");
+            System.out.println("last name length: " + lastName.length());
+        } else if(social.trim().length() != 9)
+        {
+            JOptionPane.showMessageDialog(null, "Social Security Number invalid");
+        } else
+        {
+            informationCorrect = true;
+        }
+        return informationCorrect;
+    }
 
     private void addClientInfo(String firstName, String lastName, String social, Connection bankConnection)
     {
@@ -62,5 +95,40 @@ public class CreateClientController
 
             preparedStatement.execute();
         } catch (SQLException e) { e.printStackTrace(); }
+    }
+
+
+
+    private boolean doesAccountExist()
+    {
+
+        boolean accountExists = false;
+        try
+        {
+            Statement query = bankConnection.createStatement();
+
+            String sqlQuery = "SELECT first_name, last_name, account_number FROM clients";
+
+            ResultSet resultSet = query.executeQuery(sqlQuery);
+
+            while(resultSet.next())
+            {
+
+                //account number from database
+                int accountNumberDatabase = Integer.parseInt(resultSet.getString(3));
+                int accountFromInput = view.getAcctNum();
+
+                if(accountNumberDatabase == accountFromInput)
+                {
+                    accountExists = true;
+                    System.out.println("Account found for: " + resultSet.getString(1) + " " + resultSet.getString(2));
+                    break;
+                }
+                else
+                    accountExists = false;
+            }
+        }
+        catch(SQLException e) { e.printStackTrace(); }
+        return  accountExists;
     }
 }
