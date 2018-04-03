6fa14fac162842bbd0186707b0a0f2d5152028fe
rissandimo - PC <nassiromid@gmail.com>
2018-04-02 19:59:26 -0700

    refactored - removed getBiggestAccountNumber method and put in in Utilities class cleaned code

--- src/controller/CreateClientController.java
+++ src/controller/CreateClientController.java
@@ -1,26 +1,22 @@
+
 package controller;
 
 import model.BankConnection;
-import view.AccessAccountView;
-import view.WelcomeScreen;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CreateClientController
 {
     private String firstName, lastName, social;
-    private WelcomeScreen view;
-
     private int accountNumber;
 
     public CreateClientController(String firstName, String lastName, String social)
     {
         Connection bankConnection = BankConnection.createConnection();
 
-        this.accountNumber = getBiggestAccountNumber(bankConnection);
+        this.accountNumber = Utilities.getBiggestAccountNumber(bankConnection);
         this.accountNumber++; // increment account number for next client
 
         this.firstName = firstName;
@@ -34,8 +30,6 @@ public class CreateClientController
         {
         addClientInfo(firstName, lastName, social, connection);
         addCheckingInfo(connection, accountNumber, 0.0, social);
-
-        //new AccessAccountView(accountNumber);
         }
 
     private void addClientInfo(String firstName, String lastName, String social, Connection bankConnection)
@@ -52,62 +46,21 @@ public class CreateClientController
                 preparedStatementClient.setInt(4, accountNumber);
 
                 preparedStatementClient.execute();
-                System.out.println("client info added");
             }
         }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
-        }
-    }
+        catch (SQLException e) { e.printStackTrace(); }}
 
-    private void addCheckingInfo(Connection bankConnection, int ACCOUNT_NUMBER, double balance, String social)
-    {
-
-        try
-        {
+    private void addCheckingInfo(Connection bankConnection, int ACCOUNT_NUMBER, double balance, String social) {
+        try {
             String checkingStatement = "INSERT INTO checking_account (account_number, account_balance, social) values(?,?,?)";
 
             PreparedStatement preparedStatement = bankConnection.prepareStatement(checkingStatement);
 
             preparedStatement.setInt(1, ACCOUNT_NUMBER);
-            preparedStatement.setDouble(2,balance);
+            preparedStatement.setDouble(2, 0.0);
             preparedStatement.setString(3, social);
 
             preparedStatement.execute();
-            System.out.println("checking info added");
-        }
-        catch(SQLException e)
-        {
-            e.printStackTrace();
-        }
-    }
-
-    private int getBiggestAccountNumber(Connection bankConnection)
-    {
-
-        int largestAccountNumber = 0;
-
-        try
-        {
-            String largestAcctNum = "SELECT MAX(account_number) from clients";
-
-            PreparedStatement preparedStatement = bankConnection.prepareStatement(largestAcctNum);
-
-
-            ResultSet resultLargestAcctNum = preparedStatement.executeQuery();
-
-
-            while(resultLargestAcctNum.next())
-            {
-                largestAccountNumber = resultLargestAcctNum.getInt(1);
-                System.out.println("Largest account# found: " + largestAccountNumber);
-                return largestAccountNumber;
-            }
-
-        }
-        catch(SQLException e) { e.printStackTrace(); }
-
-        return largestAccountNumber;
+        } catch (SQLException e) { e.printStackTrace(); }
     }
 }
