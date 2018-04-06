0e4991257f0de75cfb7955b0ffe1eed66e10f9f9
= <nassiromid@gmail.com>
2018-04-05 23:28:42 -0700

    Initial commit

--- src/Bank.java
+++ src/Bank.java
@@ -1,7 +1,10 @@
+import javafx.application.Application;
+import view.WelcomeScreen;
+
 public class Bank
 {
     public static void main(String[] args)
     {
-
+        Application.launch(WelcomeScreen.class, args);
     }
 }
