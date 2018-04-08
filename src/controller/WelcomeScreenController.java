5551a2b267eaa745ee3b338ea31640a19eca250f
rissandimo - PC <nassiromid@gmail.com>
2018-04-07 20:23:38 -0700

    cleaned code

--- src/controller/WelcomeScreenController.java
+++ src/controller/WelcomeScreenController.java
@@ -2,28 +2,21 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import model.BankConnection;
 import view.WelcomeScreen;
-
-import java.sql.Connection;
 
 public class WelcomeScreenController implements EventHandler<ActionEvent>
 {
-    WelcomeScreen view;
-    private Connection bankConnection;
+    private WelcomeScreen view;
 
     public WelcomeScreenController(WelcomeScreen view)
     {
         this.view = view;
-        this.bankConnection = BankConnection.createConnection();
     }
 
-        public void handle(ActionEvent event)
-        {
-            System.out.println("handle()");
-               if(event.getSource() == view.getAccessAccount()) view.setScene(view.getAccessAccountScene());
-               if(event.getSource() == view.getCreateAccount()) view.setScene(view.getCreateAccountScene());
-               if(event.getSource() == view.getDeleteAccount()) view.setScene(view.getDeleteAccountScene());
-
-        }
+    public void handle(ActionEvent event)
+    {
+        if(event.getSource() == view.getAccessAccount()) view.setScene(view.getAccessAccountScene());
+        if(event.getSource() == view.getCreateAccount()) view.setScene(view.getCreateAccountScene());
+        if(event.getSource() == view.getDeleteAccount()) view.setScene(view.getDeleteAccountScene());
+    }
 }
