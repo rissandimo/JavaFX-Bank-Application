8a503ae807c8645a1dd6a5c16b03576489c83eb3
rissandimo - PC <nassiromid@gmail.com>
2018-04-08 00:16:12 -0700

    refactored

--- src/controller/WelcomeScreenController.java
+++ src/controller/WelcomeScreenController.java
@@ -15,8 +15,8 @@ public class WelcomeScreenController implements EventHandler<ActionEvent>
 
     public void handle(ActionEvent event)
     {
-        if(event.getSource() == view.getAccessAccount()) view.setScene(view.getAccessAccountScene());
-        if(event.getSource() == view.getCreateAccount()) view.setScene(view.getCreateAccountScene());
-        if(event.getSource() == view.getDeleteAccount()) view.setScene(view.getDeleteAccountScene());
+        if(event.getSource() == view.getAccessAccountButton()) view.setScene(view.getAccessAccountScene());
+        if(event.getSource() == view.getCreateAccountButton()) view.setScene(view.getCreateAccountScene());
+        if(event.getSource() == view.getDeleteAccountButton()) view.setScene(view.getDeleteAccountScene());
     }
 }
