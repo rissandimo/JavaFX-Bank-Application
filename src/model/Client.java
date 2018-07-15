1d433ffe6a10b21220a2fd3f2ee078970b48177e
= <=>
2018-07-14 23:35:48 -0700

    Initial commit

--- src/model/Client.java
+++ src/model/Client.java
@@ -2,4 +2,25 @@ package model;
 
 public class Client
 {
+    private String firstName;
+    private String lastName;
+    private int checkingAccountNumber;
+
+    public Client(String firstName, String lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        CheckingAccount checkingAccount = new CheckingAccount();
+        //this.checkingAccountNumber = checkingAccount.generateAccountNumber();
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
 }
