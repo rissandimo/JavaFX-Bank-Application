d125134642ba90fcbe53f002f2edc5c24a56fe7f
= <=>
2018-07-14 23:34:49 -0700

    Removed ref to ListView

--- src/controller/ViewAccountsController.java
+++ src/controller/ViewAccountsController.java
@@ -1,21 +1,14 @@
 package controller;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
-import model.BankConnection;
-
-import java.sql.*;
-
 public class ViewAccountsController
 {
-    Connection connection = BankConnection.createConnection();
+    //private Connection bankConnection = BankConnection.createConnection();
 
-    @FXML
-    ListView listView;
 
     public void initialize()
     {
         System.out.println("Controller started");
     }
+
 
 }
