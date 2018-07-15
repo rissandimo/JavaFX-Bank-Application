0a65f0d6ff19ef1aaf0a1015f8c4c26cf9c03be5
= <=>
2018-07-15 10:58:34 -0700

    Added text fields references Added newTransaction() -used for testing

--- src/controller/ViewAccountsController.java
+++ src/controller/ViewAccountsController.java
@@ -1,8 +1,37 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
 public class ViewAccountsController
 {
     //private Connection bankConnection = BankConnection.createConnection();
+
+    @FXML
+    private TextField descriptionField;
+
+    @FXML
+    private TextField amountField;
+
+    @FXML
+    private ChoiceBox typeChoice;
+
+
+    @FXML
+    public void newTransaction()
+    {
+     /*   String description = descriptionField.getText();
+        double amount = Double.parseDouble(amountField.getText());*/
+        String choice = (String) typeChoice.getValue();
+
+        if(choice.equals("Deposit"))
+            System.out.println("deposit");
+        else if(choice.equals("Withdrawal"))
+            System.out.println("withdrawal");
+        else
+            System.out.println("purchase");
+    }
 
 
     public void initialize()
