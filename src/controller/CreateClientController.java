ad2bc2d372e242e23991b962d5dad0df5e85fbe1
= <=>
2018-07-14 23:33:17 -0700

    Put instance of WelcomeScreen Added method to format client name Switched try to try w/ resources

--- src/controller/CreateClientController.java
+++ src/controller/CreateClientController.java
@@ -1,6 +1,9 @@
 package controller;
 
 import model.BankConnection;
+import view.AccessAccountView;
+import view.WelcomeScreen;
+
 import java.sql.*;
 import javax.swing.*;
 
@@ -9,13 +12,14 @@ public class CreateClientController
     private String firstName, lastName, social;
     private int accountNumber;
     private Connection bankConnection;
+    private WelcomeScreen view = new WelcomeScreen();
 
     public CreateClientController(String firstName, String lastName, String social)
     {
         this.bankConnection = BankConnection.createConnection();
         this.accountNumber = Utilities.getBiggestAccountNumber(bankConnection);
         this.accountNumber++; // increment account number for next client
-
+        System.out.println("CreateClientController, accountNumber: " + accountNumber);
         this.firstName = firstName;
         this.lastName = lastName;
         this.social = social;
@@ -27,10 +31,21 @@ public class CreateClientController
         {
            if(clientInfoValid() && (!accountExists(social) ))
            {
+            formatName(firstName, lastName);
             addClientInfo(firstName, lastName, social, connection);
             addCheckingInfo(connection, accountNumber, social);
            }
+           else JOptionPane.showMessageDialog(null, "Account already exists for that social");
+
         }
+
+    private void formatName(String firstName, String lastName)
+    {
+        firstName = firstName.substring(0,1).toUpperCase() + firstName.substring(1);
+        lastName = lastName.substring(0,1).toUpperCase() + lastName.substring(1);
+        System.out.println(firstName);
+        System.out.println(lastName);
+    }
 
     private boolean clientInfoValid()
     {
@@ -38,43 +53,32 @@ public class CreateClientController
 
         social = social.replaceAll("[- ]", "");
 
-
-        if(firstName.length() == 0)
-        {
-            JOptionPane.showMessageDialog(null, "First name invalid");
-        } else if(lastName.length() == 0)
-        {
-            JOptionPane.showMessageDialog(null, "Last name invalid");
-        } else if(social.trim().length() != 9)
-        {
-            JOptionPane.showMessageDialog(null, "Social Security Number invalid");
-        } else
-        {
-            informationCorrect = true;
-        }
+        if(firstName.length() == 0) JOptionPane.showMessageDialog(null, "First name invalid");
+        else if(lastName.length() == 0) JOptionPane.showMessageDialog(null, "Last name invalid");
+        else if(social.trim().length() != 9) JOptionPane.showMessageDialog(null, "Social Security Number invalid");
+        else informationCorrect = true;
         return informationCorrect;
     }
 
     private void addClientInfo(String firstName, String lastName, String social, Connection bankConnection)
     {
+        String createClientStatement = "INSERT INTO clients (first_name, last_name, social, account_number) values (?, ?, ?, ?)";
+
         try
         {
-            String createClientStatement = "INSERT INTO clients (first_name, last_name, social, account_number) values (?, ?, ?, ?)";
+            PreparedStatement preparedStatementClient = bankConnection.prepareStatement(createClientStatement);
+            preparedStatementClient.setString(1, firstName);
+            preparedStatementClient.setString(2, lastName);
+            preparedStatementClient.setString(3, social);
+            preparedStatementClient.setInt(4, accountNumber);
 
-            try (PreparedStatement preparedStatementClient = bankConnection.prepareStatement(createClientStatement))
-            {
-                preparedStatementClient.setString(1, firstName);
-                preparedStatementClient.setString(2, lastName);
-                preparedStatementClient.setString(3, social);
-                preparedStatementClient.setInt(4, accountNumber);
-
-                preparedStatementClient.execute();
-            }
+            preparedStatementClient.execute();
         }
         catch (SQLException e) { e.printStackTrace(); }}
 
     private void addCheckingInfo(Connection bankConnection, int accountNumber, String social) {
-        try {
+        try
+        {
             String checkingStatement = "INSERT INTO checking_account (account_number, account_balance, social) values(?,?,?)";
 
             PreparedStatement preparedStatement = bankConnection.prepareStatement(checkingStatement);
@@ -84,7 +88,10 @@ public class CreateClientController
             preparedStatement.setString(3, social);
 
             preparedStatement.execute();
+
+            view.setScene(view.getAccessAccountScene());
         } catch (SQLException e) { e.printStackTrace(); }
+
     }
 
     private boolean accountExists(String socialFromInput)
@@ -106,11 +113,9 @@ public class CreateClientController
                 if(socialFromDatabase.equals(socialFromInput))
                 {
                     accountExists = true;
-                    System.out.println("Account found for: " + resultSet.getString(1) + " " + resultSet.getString(2));
-                    break;
+                   // System.out.println("Account number" + resultSet.getString(1) + "already exists");
+                    //break;
                 }
-                else
-                    accountExists = false;
             }
         }
         catch(SQLException e) { e.printStackTrace(); }
