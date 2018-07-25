ab736e46c04d26f81183818c9644708730b2085e
= <=>
2018-07-24 19:15:33 -0700

    Initial commit -Changes main scene to access account scene using the same stage

---
+++ src/controller/MainViewController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainViewController
+{
+
+    @FXML
+    public void handleSubmit(ActionEvent event) throws IOException
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("../view/viewAccounts.fxml"));
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 700,600);
+        currentStage.setScene(scene);
+        currentStage.show();
+    }
+}
