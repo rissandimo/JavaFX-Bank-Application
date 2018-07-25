a7665e3d1d572025b4f9b6657a47786ca506f95c
= <=>
2018-07-24 19:18:53 -0700

    Changed root dir path for mainView.fxml

--- src/Bank.java
+++ src/Bank.java
@@ -1,11 +1,28 @@
 import javafx.application.Application;
-import view.WelcomeScreen;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Bank
+import java.io.IOException;
+
+public class Bank extends Application
 {
-    public static void main(String[] args)
+    Stage window;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
     {
-        Application.launch(WelcomeScreen.class, args);
+        window = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("view/mainView.fxml"));
+        window.setScene(new Scene(root, 700,600));
+        window.show();
 
     }
+
+    public static void main(String[] args)
+{
+    launch(args);
+
+}
 }
