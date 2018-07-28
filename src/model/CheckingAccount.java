1b402e911fe137d0b596e1faf93167d68f36a879
= <=>
2018-07-28 16:00:49 -0700

    Added method skeletons for deposit and withdrawl

--- src/model/CheckingAccount.java
+++ src/model/CheckingAccount.java
@@ -1,8 +1,54 @@
 package model;
 
+import javafx.fxml.FXML;
+
 class CheckingAccount
 {
     private long accountNumber;
     private double balance;
 
+    @FXML
+    private void handleDeposit(double amountToDeposit)
+    {
+        if (depositValid(amountToDeposit))
+        {
+            //add to balance from db
+        }
+    }
+
+    private boolean depositValid(double amountToDeposit)
+    {
+        if(!Double.isNaN(amountToDeposit))
+        {
+            if(amountToDeposit > 0 && amountToDeposit <= 500)
+            return true;
+        }
+        return false;
+    }
+
+    @FXML
+    private void showBalance()
+    {
+        // connect to db and query balance
+    }
+
+    @FXML
+    private void handleWithdrawal(double amountToWithdraw)
+    {
+        if(withdrawValid(amountToWithdraw))
+        {
+            // withdraw from balance from db
+        }
+
+    }
+
+    private boolean withdrawValid(double amountToWithdraw)
+    {
+        if(!Double.isNaN(amountToWithdraw))
+        {
+            if(amountToWithdraw > 0 && amountToWithdraw <= 500)
+                return true;
+        }
+        return false;
+    }
 }
