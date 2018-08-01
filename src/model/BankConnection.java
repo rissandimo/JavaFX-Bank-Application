42b5f6ddb8cb0213907d1e5f92cb89609c3ea169
= <=>
2018-08-01 09:34:38 -0700

    add Strings for connection \n updated createConnection()

--- src/model/BankConnection.java
+++ src/model/BankConnection.java
@@ -1,27 +1,99 @@
 package model;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import javax.swing.*;
+import java.sql.*;
 
 public class BankConnection
 {
-    private final static String CONNECTION = "jdbc:mysql://localhost::3306/bank";
-    private final static String USERNAME = "administrator";
-    private final static String PASSWORD  = "adminPassword";
+    private final static String CONNECTION_URL = "jdbc:mysql://localhost:3306/bank";
+    private static Statement statement = null;
+    private Connection connection = null;
 
-    public static Connection createConnection()
+    private final static String USERNAME = "root";
+    private final static String PASSWORD  = "";
+
+    public BankConnection()
     {
-        Connection connection = null;
+        createConnection();
+        setupClientTable();
+    }
+
+    public void createConnection()
+    {
         try
         {
-            connection = DriverManager.getConnection(CONNECTION, USERNAME,PASSWORD);
-            return connection;
+            connection = DriverManager.getConnection(CONNECTION_URL, USERNAME,PASSWORD);
+            System.out.println("Connection successfull");
         }
-        catch(SQLException sqlException)
+        catch(SQLException e)
         {
-            sqlException.printStackTrace();
+            System.out.println("Unable to create connection");
+            e.printStackTrace();
         }
-        return null;
     }
+
+    void createDatabase()
+    {
+        try
+        {
+            String createDatabaseStatement = "CREATE DATABASE IF NOT EXISTS bank";
+            Statement statement = connection.createStatement();
+            statement.execute(createDatabaseStatement);
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    void setupClientTable()
+    {
+        String tableName = "clients";
+        String createClientTableQuery = "CREATE TABLE IF NOT EXISTS " + tableName + " ( " +
+           " first_name VARCHAR(15) NOT NULL, " +
+           " last_name VARCHAR(15) NOT NULL, " +
+           " social VARCHAR(9) NOT NULL	PRIMARY KEY )";
+        //TODO - check if table exists already
+        try
+        {
+        statement = connection.createStatement();
+        statement.execute(createClientTableQuery);
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Unable to create client table");
+        }
+    }
+
+    public boolean executeCreateClientStatement(String statement)
+    {
+        try
+        {
+            Statement statementToExecute = connection.createStatement();
+            statementToExecute.execute(statement);
+            return true;
+        }
+        catch(SQLException e)
+        {
+            return false;
+        }
+    }
+
+    ResultSet executeQuery(String queryToExecute)
+    {
+        ResultSet resultSet;
+        try
+        {
+            Statement statement = connection.createStatement();
+            resultSet = statement.executeQuery(queryToExecute);
+            return resultSet;
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Unable to execute query");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
