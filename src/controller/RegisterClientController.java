c2d2fa64575051050f46c9a8349e6629a2645566
= <=>
2018-08-01 09:51:51 -0700

    Implemented initializable Added properties for text fields Added insertNewClient() w/ Alerts for confirmation

--- src/controller/RegisterClientController.java
+++ src/controller/RegisterClientController.java
@@ -1,5 +1,62 @@
 package controller;
 
-public class RegisterClientController
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import model.BankConnection;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RegisterClientController implements Initializable
 {
+
+    @FXML
+    private JFXTextField firstNameField;
+
+    @FXML
+    private JFXTextField lastNameField;
+
+    @FXML
+    private JFXTextField socialSecurityField;
+
+    private BankConnection bankConnection;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+        bankConnection = new BankConnection();
+    }
+
+    @FXML
+    private void insertNewClient()
+    {
+        String clientFirstName = firstNameField.getText();
+        String clientLastName = lastNameField.getText();
+        String clientSocialSecurity = socialSecurityField.getText();
+
+
+
+        String createClientStatement = "INSERT INTO clients values ("  +
+                "'" + clientFirstName + "', '" + clientLastName + "', '" + clientSocialSecurity + "'" + ")";
+        if(bankConnection.executeCreateClientStatement(createClientStatement))
+        {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Account successfully created");
+            alert.setHeaderText(null);
+            alert.setContentText(clientFirstName + " " + clientLastName + " added to Bank");
+            alert.showAndWait();
+        }
+        else
+        {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Account error");
+            alert.setHeaderText(null);
+            alert.setContentText("Client " + clientFirstName + " " + clientLastName + " exists.");
+            alert.showAndWait();
+        }
+    }
+
+
 }
