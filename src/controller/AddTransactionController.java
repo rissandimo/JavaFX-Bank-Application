9f46463f400c672ca680f8f0d31bfc8ff2c56cf5
= <=>
2018-08-15 15:35:09 -0700

    Added methods: setClientInfo() handleSubmit() - adds transaction to db Replaced db conn w/ call to singleton

--- src/controller/AddTransactionController.java
+++ src/controller/AddTransactionController.java
@@ -1,10 +1,16 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.stage.Stage;
+import model.BankConnection;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,17 +19,57 @@ public class AddTransactionController implements Initializable
 {
 
     @FXML
+    private JFXTextField amountField;
+
+    @FXML
+    private JFXTextField descriptionField;
+
+    @FXML
     private ChoiceBox<String> transactionChoices = new ChoiceBox<>();
 
+    @FXML
+    private DatePicker datePicker;
+
     private ObservableList<String> transactionTypes = FXCollections.observableArrayList("Debt", "Deposit", "Withdrawal");
+
+    private BankConnection bankConnection;
+
+    private int clientAccountNumber;
+
+    private int getClientAccountNumber()
+    {
+        return clientAccountNumber;
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
+        bankConnection = BankConnection.getInstance();
         transactionChoices.getItems().setAll(transactionTypes);
     }
 
+    public void setClientInfo(int clientAccountNumber)
+    {
+        this.clientAccountNumber = clientAccountNumber;
+    }
 
+    @FXML
+    private void handleSubmit(ActionEvent event)
+    {
+        System.out.println("client account number" + getClientAccountNumber());
+        double amount = Double.parseDouble(amountField.getText());
+        String description = descriptionField.getText();
+        String transactionType = transactionChoices.getSelectionModel().getSelectedItem();
 
+        String addTransaction = "INSERT INTO " + "transactions" +
+                " (amount, trans_date, trans_type, description, balance, chk_account_number)" +
+                " values (" + amount +  ", " + "CURDATE()"  + ", '" + transactionType + "', '" +
+                description + "', " + 30.00 + ", " + clientAccountNumber + "" + ")";
+
+        bankConnection.executeStatement(addTransaction);
+
+        Stage currentStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        currentStage.close();
+    }
 
 }
