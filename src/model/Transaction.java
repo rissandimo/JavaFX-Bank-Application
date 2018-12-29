24a89d7e37bdf73825eadd0054237eed32e216bc
=Omid-PC <=nassiromid@gmail.com>
2018-12-28 18:21:27 -0800

    Removed unnecessary getters

--- src/model/Transaction.java
+++ src/model/Transaction.java
@@ -1,7 +1,6 @@
 package model;
 
 import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 
@@ -14,22 +13,6 @@ public class Transaction
     private SimpleStringProperty type;
     private SimpleStringProperty description;
     private SimpleDoubleProperty balance;
-
-    public String getSocial()
-    {
-        return social.get();
-    }
-
-    public SimpleStringProperty socialProperty()
-    {
-        return social;
-    }
-
-    public void setSocial(String social)
-    {
-        this.social.set(social);
-    }
-
     private SimpleStringProperty social;
 
     public Transaction(double amount, Date date, String type, String description, double balance, String social)
@@ -42,49 +25,9 @@ public class Transaction
         this.social = new SimpleStringProperty(social);
     }
 
-    public Date getDate()
-    {
-        return date.get();
-    }
-
-    public SimpleObjectProperty<Date> dateProperty()
-    {
-        return date;
-    }
-
-    public void setDate(Date date)
-    {
-        this.date.set(date);
-    }
-
     public String getDescription()
     {
         return description.get();
-    }
-
-    public SimpleStringProperty descriptionProperty()
-    {
-        return description;
-    }
-
-    public void setDescription(String description)
-    {
-        this.description.set(description);
-    }
-
-    public String getType()
-    {
-        return type.get();
-    }
-
-    public SimpleStringProperty typeProperty()
-    {
-        return type;
-    }
-
-    public void setType(String type)
-    {
-        this.type.set(type);
     }
 
     public double getAmount()
@@ -92,14 +35,15 @@ public class Transaction
         return amount.get();
     }
 
-    public SimpleDoubleProperty amountProperty()
+
+    public Date getDate()
     {
-        return amount;
+        return date.get();
     }
 
-    public void setAmount(double amount)
+    public String getType()
     {
-        this.amount.set(amount);
+        return type.get();
     }
 
     public double getBalance()
@@ -107,14 +51,6 @@ public class Transaction
         return balance.get();
     }
 
-    public SimpleDoubleProperty balanceProperty()
-    {
-        return balance;
-    }
 
-    public void setBalance(double balance)
-    {
-        this.balance.set(balance);
-    }
 
 }
